283f8861e3dd21b6e641816de35e3aa178e13a0e
Yehor Horbatenko <Yehor.Horbatenko@davita.com>
2021-09-21 17:40:06 +0300

    add shouldSendMPIByBindingName test method

--- src/test/java/com/softserve/patientsservice/handler/PatientStreamBridgeTest.java
+++ src/test/java/com/softserve/patientsservice/handler/PatientStreamBridgeTest.java
@@ -1,9 +1,16 @@
 package com.softserve.patientsservice.handler;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.junit.ClassRule;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,7 +18,11 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 import org.testcontainers.utility.DockerImageName;
+
+import java.time.Duration;
+import java.util.Collections;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,18 +44,46 @@ class PatientStreamBridgeTest {
     @Autowired
     PatientStreamBridge patientStreamBridge;
 
-    private final String topic = "testTopic";
+    private final String BINDING_OUT = "deactivatePatientByMPI-out-0";
 
-    private final String MPI = "010203";
+    @Value("${spring.cloud.stream.bindings." + BINDING_OUT + ".destination}")
+    private String topic;
 
     @AfterAll()
     static void tearDown() {
         kafka.close();
     }
 
-    @Test
-    void send() {
 
+    @Test
+    void shouldSendMPIByBindingName() {
+
+        final String MPI = "test-MPI-value";
+        int countOfOutputMessages = 3;
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(
+                ImmutableMap.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers(),
+                        ConsumerConfig.GROUP_ID_CONFIG, "testGroup",
+                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"
+                ),
+                new StringDeserializer(),
+                new StringDeserializer()
+        );
+
+        consumer.subscribe(Collections.singletonList(topic));
+
+        for (int i = 0; i < countOfOutputMessages; i++) {
+            patientStreamBridge.send(BINDING_OUT, MPI);
+        }
+
+        ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(2000));
+
+        Assertions.assertEquals(countOfOutputMessages, consumerRecords.count());
+
+        consumerRecords.records(topic).forEach(s -> Assertions.assertEquals(MPI, s.value()));
+
+        consumer.unsubscribe();
     }
 
 }
